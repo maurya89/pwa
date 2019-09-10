e2632c13c5b9bad06b503429530e1b61321bd1ae
Ashish Maurya <andy@123789.org>
2019-09-10 14:44:33 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 window.onload = () => {
   'use strict';
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+  if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+      console.log("[PWA Builder] active service worker found, no need to register");
+    } else {
+      // Register the service worker
+      navigator.serviceWorker
+        .register("sw.js", {
+          scope: "./"
+        })
+        .then(function (reg) {
+          console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+        });
+    }
   }
 }
